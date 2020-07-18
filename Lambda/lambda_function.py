9658a843eb38e8f20c2b6a39bec1a8c98967f9ea
greengangsta <acc.surajtripathi@gmail.com>
2020-07-18 10:08:41 +0530

    modifying lambda output

--- Lambda/lambda_function.py
+++ Lambda/lambda_function.py
@@ -1,5 +1,5 @@
 import json
 
 def lambda_handler(event, context):
-    msg = "hello world with anuj and team!!!!!"
+    msg = "hello world with anuj and team, changing lambda now after creating the s3 website hosting, xD!!!!!"
     return {"statusCode": 200, "body": json.dumps(msg)}
